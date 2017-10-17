db120c2b6e72191155ff528ddc83ae878a5d3784
王振涛 <wangzhentao@zhiweicloud.com>
2017-10-17 18:01:19 +0800

    修改es服务器链接地址

--- src/main/java/com/adatafun/recommendation/conf/ESFactory.java
+++ src/main/java/com/adatafun/recommendation/conf/ESFactory.java
@@ -20,7 +20,7 @@ public class ESFactory {
 
         JestClientFactory factory = new JestClientFactory();
         factory.setHttpClientConfig(new HttpClientConfig
-                .Builder("http://116.62.184.103:9200")
+                .Builder("http://122.224.248.26:9200") // 本地使用118.31.158.168，线上使用122.224.248.26
                 .gson(new GsonBuilder().setDateFormat("yyyy-MM-dd'T'hh:mm:ss").create())
                 .connTimeout(1500)
                 .readTimeout(3000)
